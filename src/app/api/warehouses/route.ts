cf6e11aec9b9065da230a4a3472065f0506afa98
janak-Thapa <ogjanak1@gmail.com>
2024-08-11 13:08:00 +0545

    create warehouse endpoint

---
+++ src/app/api/warehouses/route.ts
@@ -0,0 +1,29 @@
+import { db } from "@/lib/db/db";
+import { warehouses } from "@/lib/db/schema";
+import { warehouseSchema } from "@/lib/validators/warehouseSchema";
+
+export async function POST(request:Request){
+    const requestData = await request.json()
+
+    let validatedData;
+
+    try {
+        validatedData = await warehouseSchema.parse(requestData)
+        
+    } catch (error) {
+        return Response.json({message:error},{status:400})
+        
+    }
+
+
+    try {
+        await db.insert(warehouses).values(validatedData)
+
+        return Response.json({message:"OK"},{status:201})
+        
+    } catch (error) {
+        return Response.json({message:"Failed to store the warehose"},{status:500})
+        
+    }
+
+}
---
+++ src/lib/validators/warehouseSchema.ts
@@ -0,0 +1,5 @@
+import {z } from "zod"
+export const warehouseSchema = z.object({
+    name:z.string({message:"Warehouse name should be a string"}),
+    pincode:z.string({message:"pincode should be a string"}).length(6),
+})
