8f202b822dbff9fde3a681efe953f0d79030b6e5
janak-Thapa <ogjanak1@gmail.com>
2024-08-12 15:17:10 +0545

    create inventories

---
+++ src/app/api/inventories/route.ts
@@ -0,0 +1,28 @@
+import { db } from "@/lib/db/db";
+import { inventories } from "@/lib/db/schema";
+import { inventorySchema } from "@/lib/validators/inventorySchema";
+
+export async function POST(request:Request){
+    const requestData = await request.json()
+
+    let validatedData;
+
+    try {
+        validatedData = await inventorySchema.parse(requestData)
+        
+    } catch (error) {
+
+        return Response.json({message:error},{status:400})
+        
+    }
+
+    try {
+        await db.insert(inventories).values(validatedData)
+
+        return Response.json({message:"Ok"},{status:201})
+        
+    } catch (error) {
+        return Response.json({message:"Failed to store the inventory into the database"},{status:500})
+        
+    }
+}
