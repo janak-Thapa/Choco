2c37ebef5f8fc461cc27eaa291dee4fa67f70c3a
janak-Thapa <ogjanak1@gmail.com>
2024-08-12 15:23:56 +0545

    get all inventories

--- src/app/api/inventories/route.ts
+++ src/app/api/inventories/route.ts
@@ -1,6 +1,7 @@
 import { db } from "@/lib/db/db";
-import { inventories } from "@/lib/db/schema";
+import { inventories, products, warehouses } from "@/lib/db/schema";
 import { inventorySchema } from "@/lib/validators/inventorySchema";
+import { desc, eq } from "drizzle-orm";
 
 export async function POST(request:Request){
     const requestData = await request.json()
@@ -23,6 +24,23 @@ export async function POST(request:Request){
         
     } catch (error) {
         return Response.json({message:"Failed to store the inventory into the database"},{status:500})
+        
+    }
+}
+
+export async function GET(){
+    try {
+        const allInventories = await db.select({
+            id:inventories.id,
+            sku:inventories.sku,
+            warehouse:warehouses.name,
+            product:products.name,
+
+        }).from(inventories).leftJoin(warehouses,eq(inventories.warehouseId,warehouses.id)).leftJoin(products,eq(inventories.productId,products.id))
+        .orderBy(desc(inventories.id))
+         return Response.json(allInventories)        
+    } catch (error) {
+        return Response.json({message:"Failed to fetch inventories"},{status:500})
         
     }
 }
