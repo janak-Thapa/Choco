4f3128d93f37ed6883c833072d9394e5c03bc66c
janak-Thapa <ogjanak1@gmail.com>
2024-08-12 13:44:19 +0545

    get all delivery persons endpoint

--- src/app/api/delivery-persons/route.ts
+++ src/app/api/delivery-persons/route.ts
@@ -1,6 +1,8 @@
 import { db } from "@/lib/db/db";
-import { deliveryPersons } from "@/lib/db/schema";
+import { deliveryPersons, warehouses } from "@/lib/db/schema";
 import { deliveryPersonSchema } from "@/lib/validators/deliveryPersonSchema";
+import { eq } from "drizzle-orm";
+import { desc } from "drizzle-orm";
 
 export async function POST(request:Request){
     const requestData = await request.json()
@@ -21,6 +23,24 @@ export async function POST(request:Request){
         
     } catch (error) {
         return Response.json({message:"Failed to store the delivery person into the database"},{status:500})
+        
+    }
+}
+
+
+export async function GET(){
+    try {
+        const allDeliveryPersons = await db.select({
+            id:deliveryPersons.id,
+            name:deliveryPersons.name,
+            phone:deliveryPersons.phone,
+            warehouse:warehouses.name
+        }).from(deliveryPersons).leftJoin(warehouses,eq(deliveryPersons.warehouseId,warehouses.id)).orderBy(desc(deliveryPersons.id))
+
+        return Response.json({allDeliveryPersons})
+        
+    } catch (error) {
+        return Response.json({message:"Failed to fetch the delivery persons"})
         
     }
 }
