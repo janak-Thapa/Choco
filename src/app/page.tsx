ad6a275a13d43c4c7333244e83ea65449147b092
janak-Thapa <ogjanak1@gmail.com>
2024-08-13 16:18:28 +0545

    add shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
+    <>
+    
    <h1>Hello e-commerce app</h1>
+   <Button>Click me</Button>
+   </>
   );
 }
